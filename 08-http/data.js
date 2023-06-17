23bf6034bc80ff93bd8f130815188306e3c8873b
norzserge <my-metrica@yandex.ru>
2023-06-17 17:44:14 +0300

    http

---
+++ 08-http/data.js
@@ -0,0 +1,7 @@
+const comments = [
+  { id: 0, text: 'First comment', author: 'Sergey' },
+  { id: 1, text: 'Second comment', author: 'Alena' },
+  { id: 2, text: 'Third comment', author: 'Artem' },
+];
+
+module.exports = comments;
